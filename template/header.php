987c69989a2a72357409a1907e3b2f4a9976146d
Rey Harold Matanguihan <118278248+ReyHarold@users.noreply.github.com>
2025-10-03 11:18:01 +0800

    added favicon

--- template/header.php
+++ template/header.php
@@ -19,6 +19,25 @@ if (!isset($_SESSION['user_id']) ||
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   <link rel="stylesheet" href="assets/bootstrap-5.3.7-dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdn.datatables.net/2.3.4/css/dataTables.bootstrap5.min.css">
+
+  <!-- Favicon -->
+<link rel="icon" type="image/png" sizes="32x32" href="/assets/uploads/logo/favicon_io/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/assets/uploads/logo/favicon_io/favicon-16x16.png">
+<link rel="shortcut icon" href="/assets/uploads/logo/favicon_io/favicon.ico">
+
+<!-- Apple Touch Icon -->
+<link rel="apple-touch-icon" href="/assets/uploads/logo/favicon_io/apple-touch-icon.png">
+
+<!-- Android Chrome -->
+<link rel="icon" type="image/png" sizes="192x192" href="/assets/uploads/logo/favicon_io/android-chrome-192x192.png">
+<link rel="icon" type="image/png" sizes="512x512" href="/assets/uploads/logo/favicon_io/android-chrome-512x512.png">
+
+<!-- Web Manifest -->
+<link rel="manifest" href="/assets/uploads/logo/favicon_io/site.webmanifest">
+
+<!-- Optional: Theme color for Android address bar -->
+<meta name="theme-color" content="#ffffff">
+
   <style>
     a.disabled {
       pointer-events: none;
