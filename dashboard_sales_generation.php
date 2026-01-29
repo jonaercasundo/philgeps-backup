06ac2a0eb0b108683f29c7a2dab881b28aa7b80f
Ivan Medrano <medranoivan222@gmail.com>
2026-01-29 17:22:52 +0800

    Add version on dashboard_sales_generation.js

--- dashboard_sales_generation.php
+++ dashboard_sales_generation.php
@@ -264,7 +264,7 @@ if ($selectedProject > 0) {
 </script>
 
 <script src="assets/js/sortable.js"></script>
-<script src="assets/js/dashboard_sales_generation.js"></script>
+<script src="assets/js/dashboard_sales_generation.js?v=1.0.0"></script>
 
 <!-- Project List Modal -->
 <div class="modal fade" id="projectListModal" tabindex="-1" aria-labelledby="projectListModalLabel" aria-hidden="true">
