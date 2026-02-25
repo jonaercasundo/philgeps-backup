dc5d1309358da8b0e1667ee50f4f3a074e0a0b01
Rey Harold Matanguihan <118278248+ReyHarold@users.noreply.github.com>
2026-02-25 16:18:28 +0800

    Update update_ar_settings.php

--- update_ar_settings.php
+++ update_ar_settings.php
@@ -4,11 +4,16 @@ require "config/db.php";
 $project_id = $_POST['project_id'];
 
 $project_name = trim($_POST['project_name']);
-$company = trim($_POST['company']);
-$client = trim($_POST['client']);
+$company      = trim($_POST['company']);
+$client       = trim($_POST['client']);
 
-$display_label = isset($_POST['display_label']) ? 1 : 0;
-$display_school_id = isset($_POST['display_school_id']) ? 1 : 0;
+$display_label      = isset($_POST['display_label']) ? 1 : 0;
+$display_school_id  = isset($_POST['display_school_id']) ? 1 : 0;
+
+$label_school_id    = isset($_POST['label_school_id']) ? 1 : 0;
+$label_municipality = isset($_POST['label_municipality']) ? 1 : 0;
+$label_division     = isset($_POST['label_division']) ? 1 : 0;
+$label_region       = isset($_POST['label_region']) ? 1 : 0;
 
 try {
 
@@ -19,7 +24,11 @@ try {
             company = ?,
             client = ?,
             display_label = ?,
-            display_school_id = ?
+            display_school_id = ?,
+            label_school_id = ?,
+            label_municipality = ?,
+            label_division = ?,
+            label_region = ?
         WHERE project_id = ?
     ");
 
@@ -29,6 +38,10 @@ try {
         $client,
         $display_label,
         $display_school_id,
+        $label_school_id,
+        $label_municipality,
+        $label_division,
+        $label_region,
         $project_id
     ]);
 
