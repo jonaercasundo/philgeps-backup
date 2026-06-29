1b35c4aff93d2998c324edb0818bbf467d7400fa
Jonaer Casundo <jcasundo.sedge@gmail.com>
2026-06-29 15:39:25 +0800

    updated code 29062026339

--- entry.php
+++ entry.php
@@ -10,19 +10,11 @@ $delivery_id = $_GET['delivery_id'] ?? null;
   <script>
     const id = "<?= $id ?>";
     const delivery_id = "<?= $delivery_id ?>";
-    
-    if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/philgeps/sw.js')
-        .then(reg => console.log('Service Worker registered:', reg.scope))
-        .catch(err => console.error('Service Worker registration failed:', err));
-    }
-    if (navigator.onLine) {
-      // If online → continue to scan.php with same params
-      window.location.href = `scan.php?id=${id}&delivery_id=${delivery_id}`;
-    } else {
-      // If offline → go to offline page
-      window.location.href = `offline_scan.php?id=${id}&delivery_id=${delivery_id}`;
-    }
+
+    // ALWAYS go to scan.php first (QR MUST be deterministic)
+    window.location.replace(
+      `scan.php?id=${id}&delivery_id=${delivery_id}`
+    );
   </script>
 </head>
 <body>
